d1f6986194f5abf5849ecb8b96ba7bc14225662c
Rohit Rawat <75693046+rohitrawatcse@users.noreply.github.com>
2022-06-20 09:47:00 +0530

    updated

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title>PHP CRUD Operation Using Google Firebase - NiceSnippets.com</title>
+    <title>PHP CRUD Operation</title>
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
